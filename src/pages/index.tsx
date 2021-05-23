890c68808f43c9ca75c915e5748bf4b2f4964384
Fillipi Pereira <wenanzer@gmx.de>
2021-05-23 06:12:30 +0200

    API-oppsett

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,39 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss'
+
+export default function Home(props) {
+
   return (
     <>
       <Head>
         <title>Begynne | tenningNyheter</title>
       </Head>
-      <main>
-        <section>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
           <span>👏 Hei, velkommen!</span>
+          <h1>Nyheter om <span>React</span> verdenen.</h1>
+          <p>
+            Få tilgang til alle publikasjonene <br />
+            <span>for kr 89,90 måned</span>
+          </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Jentekoding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      navn: 'Fillipi'
+    }
+  }
 }
