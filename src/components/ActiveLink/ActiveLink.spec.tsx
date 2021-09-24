77ca7a23bd9315e37d641c2521c5e4d18c61db6d
Fillipi Pereira <pfillipi@gmx.de>
2021-09-24 15:08:19 +0200

    Mock

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,6 +1,8 @@
 import { render } from "@testing-library/react";
 import { ActiveLink } from ".";
 
+jest.mock("next/router");
+
 test("active link renders correctly", () => {
   const { debug } = render(
     <ActiveLink href="/" activeClassName="active">
