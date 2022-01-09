2b8ac5f780776e7af8abf8797d421ce74ba42432
Fillipi Pereira <pfillipi@gmx.de>
2022-01-09 15:10:02 +0100

    return

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import { ActiveLink } from ".";
 
-jest.mock("next/router");
+jest.mock("next/router", () => {});
 
 test("active link renders correctly", () => {
   const { debug } = render(
