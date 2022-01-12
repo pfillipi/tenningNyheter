fc00a65a5644f1c67448ae9deb1af11ce4142900
Fillipi Pereira <pfillipi@gmx.de>
2022-01-12 23:52:01 +0100

    Test 2

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -20,3 +20,13 @@ test("active link renders correctly", () => {
 
   expect(getByText("Home")).toBeInTheDocument();
 });
+
+test("active link is receiving active class", () => {
+  const { getByText } = render(
+    <ActiveLink href="/" activeClassName="active">
+      <a>Home</a>
+    </ActiveLink>
+  );
+
+  expect(getByText("Home")).toBeInTheDocument();
+});
