85cf414233e9ee275ed48d6f7bf18a0ce9bf0b5b
Fillipi Pereira <pfillipi@gmx.de>
2022-01-13 00:30:34 +0100

    it

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -12,7 +12,7 @@ jest.mock("next/router", () => {
 });
 
 describe("ActiveLink component", () => {
-  test("active link renders correctly", () => {
+  it("active link renders correctly", () => {
     const { getByText } = render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
@@ -22,7 +22,7 @@ describe("ActiveLink component", () => {
     expect(getByText("Home")).toBeInTheDocument();
   });
 
-  test("active link is receiving active class", () => {
+  it("active link is receiving active class", () => {
     const { getByText } = render(
       <ActiveLink href="/" activeClassName="active">
         <a>Home</a>
