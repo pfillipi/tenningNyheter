ae6a39168bcdf6d45cbff72bbb8a20ccc8aa1dbf
Fillipi Pereira <pfillipi@gmx.de>
2022-02-23 16:14:11 +0100

    Button

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,13 +1,8 @@
 import { render, screen } from "@testing-library/react";
+import { useSession } from "next-auth/client";
 import { SignInButton } from ".";
 
-jest.mock("next-auth/client", () => {
-  return {
-    useSession() {
-      return [null, false];
-    },
-  };
-});
+jest.mock("next-auth/client");
 
 describe("SignInButton component", () => {
   it("renders correctly when user is not authenticated", () => {
