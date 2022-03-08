ba5d3616e51458c91d23763ff56402941a106e28
Fillipi Pereira <pfillipi@gmx.de>
2022-03-08 08:24:42 +0100

    fake-price-id

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -3,6 +3,6 @@ import Home from "../../pages";
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    render(<Home />);
+    render(<Home product={{ priceId: "fake-price-id" }} />);
   });
 });
