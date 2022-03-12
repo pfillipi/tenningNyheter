2b27f4a350e8122cec80334df886399bd9cc257a
Fillipi Pereira <pfillipi@gmx.de>
2022-03-12 08:36:35 +0100

    amount

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -3,6 +3,6 @@ import Home from "../../pages";
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    render(<Home product={{ priceId: "fake-price-id", amount }} />);
+    render(<Home product={{ priceId: "fake-price-id", amount: "" }} />);
   });
 });
