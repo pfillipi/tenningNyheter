6c7119a476efaafa86649216a049fcb5ef9eb183
Fillipi Pereira <pfillipi@gmx.de>
2022-03-16 09:09:15 +0100

    //

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -3,8 +3,8 @@ import Home from "../../pages";
 
 describe("Home page", () => {
   it("renders correctly", () => {
-    render(<Home product={{ priceId: "fake-price-id", amount: "10.00 kr" }} />);
+    render(<Home product={{ priceId: "fake-price-id", amount: "10.00kr" }} />);
 
-    expect(screen.getByText("for 10.00 kr")).toBeInTheDocument();
+    expect(screen.getByText("/10.00kr/")).toBeInTheDocument();
   });
 });
