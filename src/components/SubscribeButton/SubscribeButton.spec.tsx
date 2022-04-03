7a310f1d0c25dc9f382b26dce0c058d5e24d67ca
Fillipi Pereira <pfillipi@gmx.de>
2022-04-03 10:56:30 +0200

    Subscribe

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { mocked } from "ts-jest/utils";
 import { useSession } from "next-auth/client";
-import { SignInButton } from ".";
+import { SubscribeButton } from ".";
 
 jest.mock("next-auth/client");
 
@@ -14,21 +14,5 @@ describe("SignInButton component", () => {
     render(<SignInButton />);
 
     expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
-  });
-
-  it("renders correctly when user is authenticated", () => {
-    const useSessionMocked = mocked(useSession);
-
-    useSessionMocked.mockReturnValueOnce([
-      {
-        user: { name: "John Doe", email: "john.doe@example.com" },
-        expires: "fake-expires",
-      },
-      false,
-    ]);
-
-    render(<SignInButton />);
-
-    expect(screen.getByText("John Doe")).toBeInTheDocument();
   });
 });
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,36 +1,32 @@
-import { useSession, signIn } from 'next-auth/client';
-import { useRouter } from 'next/router';
-import { api } from '../../services/api';
-import { getStripeJs } from '../../services/stripe-js';
-import styles from './styles.module.scss';
+import { useSession, signIn } from "next-auth/client";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
-interface SubscribeButtonProps {
-  priceId: string;
-}
-
-export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+export function SubscribeButton() {
   const [session] = useSession();
-  const router = useRouter()
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
-      signIn('github')
+      signIn("github");
       return;
     }
 
     if (session.activeSubscription) {
-      router.push('/posts');
+      router.push("/posts");
       return;
     }
 
     try {
-      const response = await api.post('/subscribe')
+      const response = await api.post("/subscribe");
 
       const { sessionId } = response.data;
 
-      const stripe = await getStripeJs()
+      const stripe = await getStripeJs();
 
-      await stripe.redirectToCheckout({ sessionId })
+      await stripe.redirectToCheckout({ sessionId });
     } catch (err) {
       alert(err.message);
     }
