b076923eb48801f0e4b1ceabe1d4c43877f925a3
Fillipi Pereira <pfillipi@gmx.de>
2022-04-15 19:55:57 +0200

    client

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Home from "../../pages";
 
 jest.mock("next/router/index");
-jest.mock("next-auth");
+jest.mock("next-auth/client");
 
 describe("Home page", () => {
   it("renders correctly", () => {
